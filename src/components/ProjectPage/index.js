ffa89429caf8b9f2302f6200d14caede5511b7e0
saias55 <gudiguntasai77@gmail.com>
2024-01-05 05:45:24 +0000

    first commit2

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -3,12 +3,10 @@ import './index.css'
 const ProjectPage = props => {
   const {eachItem, number} = props
   const {image, name, link, description} = eachItem
-  return (
-    <li className="displayFlex" id="projectPage">
-      <div>
-        <img src={image} alt={name} className="imageDetails" />
-      </div>
-      <div className="bgc-details">
+  const firstImage = () => (
+    <div className="displayFlex">
+      <img src={image} alt={name} className="imageDetails" />
+      <div className="margin-leftD">
         <p className="addingBgc">Project {number + 1}</p>
         <h1>{name}</h1>
         <p className="description-width-details">{description}</p>
@@ -17,6 +15,43 @@ const ProjectPage = props => {
             View Project
           </button>
         </a>
+      </div>
+    </div>
+  )
+
+  const lastImage = () => (
+    <div className="displayFlex">
+      <div className="margin-rightD">
+        <p className="addingBgc">Project {number + 1}</p>
+        <h1>{name}</h1>
+        <p className="description-width-details">{description}</p>
+        <a href={link} target="__blank">
+          <button type="button" className="anchorButton">
+            View Project
+          </button>
+        </a>
+      </div>
+      <img src={image} alt={name} className="imageDetails" />
+    </div>
+  )
+
+  return (
+    <li className="displayFlex">
+      <div className="itemsOfLarge">
+        {number % 2 === 0 ? lastImage() : firstImage()}
+      </div>
+      <div className="smallScale">
+        <img src={image} alt={name} className="imageDetails" />
+        <div>
+          <p className="addingBgc">Project {number + 1}</p>
+          <h1>{name}</h1>
+          <p className="description-width-details">{description}</p>
+          <a href={link} target="__blank">
+            <button type="button" className="anchorButton">
+              View Project
+            </button>
+          </a>
+        </div>
       </div>
     </li>
   )
